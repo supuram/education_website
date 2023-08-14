5e2c1576d135b937d1f595abf2c8fee81c672d11
supuram <utsa.rkmv@gmail.com>
2023-08-14 09:59:52 +0530

    14-8-2023-again3

--- src/Home/Middlebar/Middlebar.js
+++ src/Home/Middlebar/Middlebar.js
@@ -14,7 +14,7 @@ export default function Middlebar(){
       }, []);
     return(
         <div className="divMiddlebar">
-            <img src={student1} alt='' style={{width:'79rem', marginTop:'1rem'}}></img>
+            <img src={student1} alt='' style={{width:'79rem', marginTop:'1rem'}} className="imgMiddlebar"></img>
             <div className="textOverlaydivMiddlebar">
                 <h1 className="animatedHeadingMiddlebar">BUILD</h1>
                 <h1 className="animatedHeadingMiddlebar">CREATE</h1>
